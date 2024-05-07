708123953ace907b61d4b47733ed4f7c6c7953e9
Surya Arun <138878905+Suryarun@users.noreply.github.com>
2024-05-07 14:24:42 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './App.jsx'
+// import Page from './fill.jsx'
+import './index.css'
+// import './fill.css'
+
+ReactDOM.render(<Header />, document.getElementById("root"))
+
